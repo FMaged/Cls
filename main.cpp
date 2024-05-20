daa99d92e3651fb215b7194e3a634effad2bf604
FMaged <fadelmaged25@gmail.com>
2024-05-20 14:47:12 +0200

    modifie the main.cpp

--- main.cpp
+++ main.cpp
@@ -3,17 +3,10 @@
 #include "ClsPerson.hh"
 
 int main(){
-<<<<<<< HEAD
-ClsPerson Person1("Yousif","Akbar","Yousif@Akbar.de","0987654321");
-
-Person1.print();
 
 
-
-
-=======
 string str="Hallo mein name is ubuntu";
->>>>>>> ClsString
+
 
 cout<<ClsString::reverseWordInString(str)<<endl;
     return 0;
