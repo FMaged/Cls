53bb837b0c935e17be0fedbc5143875cea347d8a
FMaged <fadelmaged25@gmail.com>
2024-05-21 00:37:55 +0200

    setting up the ClsDate class

--- ClsDate.cpp
+++ ClsDate.cpp
@@ -1,1 +1,85 @@
 #include "ClsDate.hh"
+
+
+ClsDate::ClsDate(){
+    time_t t=time(0);
+    tm* now=localtime(&t);
+    _day=now->tm_mday;
+    _month=now->tm_mon;
+    _year=now->tm_year;
+}
+ClsDate::ClsDate(short Day,short Month,short Year){
+    _day=Day;
+    _month=Month;
+    _year=Year;
+}
+ClsDate::ClsDate(string Date){
+
+
+
+}
+ClsDate::ClsDate(short dateOrderInYear, short Year){
+
+
+
+}
+
+
+short ClsDate::getDay(){
+
+    return _day;
+}
+short ClsDate::getMonth(){
+    return _month;
+}
+short ClsDate::getYear(){
+    return _year;
+}
+void ClsDate::setDate(short Day,short Month,short Year){
+    _day=Day;
+    _month=Month;
+    _year=Year;
+}
+void ClsDate::setDate(ClsDate Date){
+    _day=Date._day;
+    _month=Date._month;
+    _year=Date._year;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- ClsDate.hh
+++ ClsDate.hh
@@ -2,6 +2,9 @@
 #include "ClsString.hh"
 
 class ClsDate{
+
+private:
+
 short _day;
 short _month;
 short _year;
@@ -16,7 +19,12 @@ public:
     ClsDate(string Date);
     ClsDate(short dateOrderInYear, short Year);
 
-
+    short getDay();
+    short getMonth();
+    short getYear();
+    void setDate(short Day,short Month,short Year);
+    void setDate(ClsDate Date);
+    
 
 
 };
