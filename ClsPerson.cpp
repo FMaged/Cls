1d032aaa1095ec0eeabd1aad4a7eaffd7809e4fd
FMaged <fadelmaged25@gmail.com>
2024-05-20 02:38:24 +0200

    Add the SET and GET

--- ClsPerson.cpp
+++ ClsPerson.cpp
@@ -1,3 +1,36 @@
+#include "ClsPerson.hh"
+
+
+void ClsPerson::setFirstName(string FirstName){
+    _firstName=FirstName;
+}
+void ClsPerson::setLastName(string LastName){
+    _lastName=LastName;
+}
+void ClsPerson::setEmail(string Email){
+    _email=Email;
+}
+void ClsPerson::setPhone(string Phone){
+    _phone=Phone;
+}
+
+
+string ClsPerson::getFirstName(){
+    return _firstName;
+}
+string ClsPerson::getLastName(){
+    return _lastName;
+}
+string ClsPerson::getEmail(){
+    return _email;
+}
+string ClsPerson::getPhone(){
+    return _phone;
+}
+
+
+
+
 
 
 
